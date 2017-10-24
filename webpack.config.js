248b8b1678a4c66fe40e430c491c13e8c5f25301
Robert Dimitrov <robert_dimitrov@me.com>
2017-10-24 15:38:04 +0300

    Removed webpack-dev-middleware from dependencies.

--- webpack.config.js
+++ webpack.config.js
@@ -36,8 +36,7 @@ module.exports = {
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
+        path: path.resolve(__dirname, 'dist')
     },
     externals: nodeModules
 };
