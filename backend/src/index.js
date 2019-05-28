f4ecfdbd565eabd4263f472387ac2c2dd46a9229
Robert Dimitrov <robert_dimitrov@me.com>
2019-05-28 07:53:08 +0300

    Restored configurability of port number

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ import { DBClient } from './services/db-client';
 const dbUrl = process.env.DB_URI || '';
 
 const dbClient = new DBClient(dbUrl);
-const server = new Server(dbClient);
+const server = new Server(3000, dbClient);
 
 if (!module.parent) {
   server.start();
