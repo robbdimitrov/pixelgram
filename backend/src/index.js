02882fb9d87aab9cbc69e02344fa16a308dc2e8d
Robert Dimitrov <robert_dimitrov@me.com>
2019-05-28 19:34:15 +0300

    Moved backend port to env variable

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
 import { Server } from './server';
 import { DBClient } from './services/db-client';
 
-const dbUrl = process.env.DB_URI || '';
+const port = process.env.PORT || '';
+const dbUrl = process.env.DATABASE_URI || '';
 
 const dbClient = new DBClient(dbUrl);
-const server = new Server(3000, dbClient);
+const server = new Server(port, dbClient);
 
 if (!module.parent) {
   server.start();
