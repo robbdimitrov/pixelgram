c13b47d074f2accd21d9eb91a9a26b99bb4fecbd
Robert Dimitrov <robert_dimitrov@me.com>
2019-11-24 22:03:51 +0200

    Rename DATABASE_URI to DATABASE_URL

--- src/backend/src/index.js
+++ src/backend/src/index.js
@@ -2,7 +2,7 @@ const Server = require('./server');
 const DBClient = require('./services/db-client');
 
 const port = process.env.PORT;
-const dbUrl = process.env.DATABASE_URI;
+const dbUrl = process.env.DATABASE_URL;
 const imageDir = process.env.IMAGE_DIR || '/data/images';
 
 const dbClient = new DBClient(dbUrl);
