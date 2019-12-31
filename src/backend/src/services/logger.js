50a6c2a84853c377419cc17795fa604ca165d8ec
Robert Dimitrov <robert_dimitrov@me.com>
2019-12-31 11:33:48 +0200

    Add logger service in backend
    - Update node versions in Dockerfiles

---
+++ src/backend/src/services/logger.js
@@ -0,0 +1,19 @@
+function timeFormat() {
+  const date = new Date();
+  return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()} ` +
+    `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.` +
+    `${date.getMilliseconds()}`
+}
+
+function logError(message) {
+  process.stderr.write(`[${timeFormat()}] ${message}\n`);
+}
+
+function logInfo(message) {
+  process.stdout.write(`[${timeFormat()}] ${message}\n`);
+}
+
+module.exports = {
+  logError,
+  logInfo
+};
