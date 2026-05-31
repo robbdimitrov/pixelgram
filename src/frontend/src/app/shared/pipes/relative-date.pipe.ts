e31d938dc3e2c529c0e8b0f319303637de946d05
Robert Dimitrov <robert_dimitrov@me.com>
2026-05-31 09:55:24 +0300

    Remove frontend build warnings

--- src/frontend/src/app/shared/pipes/relative-date.pipe.ts
+++ src/frontend/src/app/shared/pipes/relative-date.pipe.ts
@@ -1,13 +1,43 @@
 import {Pipe, PipeTransform} from '@angular/core';
 
-import {dateString}  from 'quartzite';
-
 @Pipe({
     name: 'relativedate',
     standalone: false
 })
 export class RelativeDatePipe implements PipeTransform {
-  transform(value: Date): string {
-    return dateString(value);
+  private readonly formatter = new Intl.RelativeTimeFormat('en', {
+    numeric: 'auto',
+    style: 'narrow'
+  });
+
+  transform(value: Date | string): string {
+    const date = new Date(value);
+    const seconds = Math.round((date.getTime() - Date.now()) / 1000);
+    const absoluteSeconds = Math.abs(seconds);
+
+    if (absoluteSeconds < 5) {
+      return 'now';
+    }
+
+    const divisions = [
+      {amount: 60, name: 'second'},
+      {amount: 60, name: 'minute'},
+      {amount: 24, name: 'hour'},
+      {amount: 7, name: 'day'},
+      {amount: 4.34524, name: 'week'},
+      {amount: 12, name: 'month'},
+      {amount: Number.POSITIVE_INFINITY, name: 'year'}
+    ] as const;
+
+    let duration = seconds;
+    for (const division of divisions) {
+      if (Math.abs(duration) < division.amount) {
+        return this.formatter.format(Math.round(duration), division.name);
+      }
+
+      duration /= division.amount;
+    }
+
+    return this.formatter.format(Math.round(duration), 'year');
   }
 }
