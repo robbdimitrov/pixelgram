2ea7165e48d9bb4b6cf65175a2d7236cb218a2f2
Robert Dimitrov <robert_dimitrov@me.com>
2026-06-20 15:50:54 +0300

    chore: scaffold sveltekit frontend with tailwind/daisyui

---
+++ apps/frontend-svelte/svelte.config.js
@@ -0,0 +1,26 @@
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter(),
+		csp: {
+			mode: 'nonce',
+			directives: {
+				'default-src': ['self'],
+				'script-src': ['self'],
+				'style-src': ['self', 'unsafe-inline'],
+				'img-src': ['self'],
+				'connect-src': ['self'],
+				'font-src': ['self'],
+				'object-src': ['none'],
+				'base-uri': ['self'],
+				'form-action': ['self']
+			}
+		}
+	}
+};
+
+export default config;
